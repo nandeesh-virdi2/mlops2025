7134d562b68908996255d76a4d1c2e0f70466443
nandeesh-virdi2 <161629102+nandeesh-virdi2@users.noreply.github.com>
2025-04-24 11:05:20 +0530

    Create demo_1.py

---
+++ demo_1.py
@@ -0,0 +1,7 @@
+# UPloading  a random code to the github server
+
+def say_hello():
+    print("Hello, World!")
+
+say_hello()
+# This is a simple function that prints "Hello, World!" to the console.
